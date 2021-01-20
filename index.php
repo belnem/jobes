a1e2309f34937d8cf0a9708030bb3eb6655c4e53
Igor Belnem <igorbelnem@Igors-iMac.hitronhub.home>
2021-01-19 23:57:07 -0800

    New php file

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pHP</title>
+</head>
+<body>
+    <main>
+        <p>Submit a form</p>
+        <form class="submission-form" action="submissionform.php" method="post">
+            <input type="text" name="name" placeholder="Full name">
+            <input type="text" name="email" placeholder="Email">
+            <input type="text" name="title" placeholder="Job title">
+            <textarea name="message" placeholder="Message"> </textarea>
+            <button type ="submit" name="Submit">Submit form</button>
+        </form>
+    </main>
+</body>
+</html>
